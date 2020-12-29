80291e80f9ce24b970b9426a329be60a1b686094
AugustoSavi <guto_savi@outlook.com>
2020-12-29 00:05:05 -0300

    adicionado modal

--- js/services/appService.js
+++ js/services/appService.js
@@ -1,7 +1,7 @@
 angular.module('exoplanets').factory('exoplanetsApi', function($http){
 
     var _getExoplanets = function(){
-
+        $http.get("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_hostname,ra,dec&order=dec&format=json")
     }
     return{
         getExoplanets:_getExoplanets
